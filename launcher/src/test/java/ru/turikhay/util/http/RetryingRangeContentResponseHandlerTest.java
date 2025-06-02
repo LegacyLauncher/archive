eba8ebc7070221576c21878cd890fbe3259b63ef
McModder <me@modder.pw>
2025-06-03 02:24:11 +0500

    1.159.14

--- launcher/src/test/java/ru/turikhay/util/http/RetryingRangeContentResponseHandlerTest.java
+++ launcher/src/test/java/ru/turikhay/util/http/RetryingRangeContentResponseHandlerTest.java
@@ -3,6 +3,7 @@ package ru.turikhay.util.http;
 import org.apache.http.client.fluent.Executor;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.util.ByteArrayBuffer;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class RetryingRangeContentResponseHandlerTest {
 
     @Test
+    @Disabled
     void test() throws IOException {
         final String url = "https://cdn.turikhay.ru/tlauncher/legacy_beta/bootstrap.json";
         Executor executor = Executor.newInstance();
